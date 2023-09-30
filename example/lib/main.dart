1a89562a72bccfcc2805612441feefa0e31f2da2
lipeng <1162423147@qq.com>
2023-09-30 22:19:27 +0800

    [modify] 修改resource path

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phoenix_popup/phoenix_popup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -76,8 +77,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    GlobalKey globalKey = GlobalKey();
+    GlobalKey globalKey2 = GlobalKey();
     return Scaffold(
       appBar: AppBar(
+        key: globalKey,
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
@@ -86,37 +90,79 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+          color: Colors.red,
+          child: Center(
+            // key: globalKey,
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //
+              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+              // action in the IDE, or press "p" in the console), to see the
+              // wireframe for each widget.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                TextButton(
+                    onPressed: () {
+                      OverlayWindow.showOverlayWindow(context, globalKey,
+                          content: ListView.separated(
+                            itemCount: 25,
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                tileColor: Colors.blue,
+                                title: Text('item $index'),
+                              );
+                            },
+                          ));
+                    },
+                    child: const Text('test overlay window')),
+                TextButton(
+                    key: globalKey2,
+                    onPressed: () {
+                      PopupWindow.showPopWindow(
+                        context,
+                        'popup window',
+                        globalKey2,
+                        widget: SizedBox.fromSize(
+                            size: const Size(60, 44),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  'item01',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  'item02',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            )),
+                      );
+                    },
+                    child: const Text('test popup window'))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            TextButton(
-                onPressed: () {}, child: const Text('test overlay window'))
-          ],
-        ),
-      ),
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
